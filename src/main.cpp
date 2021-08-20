b990f7fff8e0220a53f3b550732a40324199ca80
34-anish <luckyanish47@gmail.com>
2021-08-20 15:39:37 +0545

    Spacing error is not solved

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,17 @@ bool isOperator(char character)
     }
     return false;
 }
+bool isSpace(char character)
+{
+    return (character == ' ');
+}
 
 std::string convertToPostfix(std::string prefix)
 {
-
+    prefix += " ";
     int length = prefix.size();
     Stack *expression = new ArrayStack(length);
+    int without_space=0;
     for (int i = length - 1; i >= 0; i--)
     {
         if (isOperator(prefix[i]))
@@ -25,12 +30,30 @@ std::string convertToPostfix(std::string prefix)
             expression->pop();
             std::string op2 = expression->top();
             expression->pop();
-            std::string temp = op1 + op2 + prefix[i];
+            std::string temp;
+            if (i != 0)
+            {
+                temp = op1 + op2 + prefix[i] + " ";
+            }
+            else
+            {
+                temp = op1 + op2 + prefix[i];
+            }
 
             expression->push(temp);
+            i--;
         }
         else
-            expression->push(std::string(1, prefix[i]));
+        {
+
+            if (isSpace(prefix[i]))
+            {
+                std::string to_push = prefix.substr(i + 1, without_space);
+                expression->push(to_push);
+                without_space = 0;
+            }
+            without_space++;
+        }
     }
     std::cout << "****************************" << std::endl;
     std::string postfix = "";
@@ -46,15 +69,18 @@ int main()
     try
     {
         // std::string prefix = "- - * a b * / c + d * e f g h";
+
         std::string prefix;
-        std::cout <<"Prefix Expression:" << std::endl;
-        getline(std::cin,prefix);
+
+        std::cout << "Prefix Expression:" << std::endl;
+        getline(std::cin, prefix);
+
         std::cout << "Prefix expression : " << prefix << std::endl;
         std::cout << "Postfix expression : " << convertToPostfix(prefix);
         // std::cout << isOperator('a') << std::endl;
     }
     catch (const char *error)
     {
-      std::cerr << error << std::endl;
+        std::cerr << error << std::endl;
     }
 }
